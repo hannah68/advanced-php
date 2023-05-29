4089f6959ed7c0814b2b6da8c0cf23e416f16ff9
hannah68 <reihaneh.naderi1989@gmail.com>
2023-05-29 11:32:52 +0100

    learn dates

--- advance.php
+++ advance.php
@@ -3,6 +3,28 @@ echo "Today is " . date("Y/m/d") . "<br>";
 echo "Today is " . date("l"). "<br>";
 echo "Year " . date("Y"). "<br>";
 echo "Today time is " . date("h:i:sa"). "<br>";
+echo "Today time is " . date("H:i:s"). "<br>";
+
 $d=mktime(11, 14, 54, 8, 12, 2014);
 echo "Created date is " . date("Y-m-d h:i:sa", $d). "<br>";
+
+$d=strtotime("10:30pm April 15 2014");
+echo "Created date is " . date("Y-m-d h:i:sa", $d). "<br>";
+
+$d=strtotime("tomorrow");
+echo date("Y-m-d h:i:sa", $d) . "<br>";
+
+$d=strtotime("next Saturday");
+echo date("Y-m-d h:i:sa", $d) . "<br>";
+
+$d=strtotime("+3 Months");
+echo date("Y-m-d h:i:sa", $d) . "<br>";
+
+$startdate = strtotime("Saturday");
+$enddate = strtotime("+6 weeks", $startdate);
+
+while ($startdate < $enddate) {
+  echo date("M d", $startdate) . "<br>";
+  $startdate = strtotime("+1 week", $startdate);
+}
 ?>
