29a23aa48a01eccb23f9a8db8d8c0f7428125034
hannah68 <reihaneh.naderi1989@gmail.com>
2023-05-29 12:11:47 +0100

    learn files functionality

--- advance.php
+++ advance.php
@@ -1,30 +1,40 @@
 <?php
-echo "Today is " . date("Y/m/d") . "<br>";
-echo "Today is " . date("l"). "<br>";
-echo "Year " . date("Y"). "<br>";
-echo "Today time is " . date("h:i:sa"). "<br>";
-echo "Today time is " . date("H:i:s"). "<br>";
+// echo "Today is " . date("Y/m/d") . "<br>";
+// echo "Today is " . date("l"). "<br>";
+// echo "Year " . date("Y"). "<br>";
+// echo "Today time is " . date("h:i:sa"). "<br>";
+// echo "Today time is " . date("H:i:s"). "<br>";
 
-$d=mktime(11, 14, 54, 8, 12, 2014);
-echo "Created date is " . date("Y-m-d h:i:sa", $d). "<br>";
+// $d=mktime(11, 14, 54, 8, 12, 2014);
+// echo "Created date is " . date("Y-m-d h:i:sa", $d). "<br>";
 
-$d=strtotime("10:30pm April 15 2014");
-echo "Created date is " . date("Y-m-d h:i:sa", $d). "<br>";
+// $d=strtotime("10:30pm April 15 2014");
+// echo "Created date is " . date("Y-m-d h:i:sa", $d). "<br>";
 
-$d=strtotime("tomorrow");
-echo date("Y-m-d h:i:sa", $d) . "<br>";
+// $d=strtotime("tomorrow");
+// echo date("Y-m-d h:i:sa", $d) . "<br>";
 
-$d=strtotime("next Saturday");
-echo date("Y-m-d h:i:sa", $d) . "<br>";
+// $d=strtotime("next Saturday");
+// echo date("Y-m-d h:i:sa", $d) . "<br>";
 
-$d=strtotime("+3 Months");
-echo date("Y-m-d h:i:sa", $d) . "<br>";
+// $d=strtotime("+3 Months");
+// echo date("Y-m-d h:i:sa", $d) . "<br>";
 
-$startdate = strtotime("Saturday");
-$enddate = strtotime("+6 weeks", $startdate);
+// $startdate = strtotime("Saturday");
+// $enddate = strtotime("+6 weeks", $startdate);
 
-while ($startdate < $enddate) {
-  echo date("M d", $startdate) . "<br>";
-  $startdate = strtotime("+1 week", $startdate);
-}
+// while ($startdate < $enddate) {
+//   echo date("M d", $startdate) . "<br>";
+//   $startdate = strtotime("+1 week", $startdate);
+// }
+
+
+// The fread() function reads from an open file.
+// The fclose() function is used to close an open file.
+// The fgets() function is used to read a single line from a file.
+// The feof() function checks if the "end-of-file" (EOF) has been reached.
+// The fgetc() function is used to read a single character from a file.
+// The fopen() function is also used to create and open a file.
+// The fwrite() function is used to write to a file.
+// You can append data to a file by using the "a" mode. The "a" mode appends text to the end of the file, while the "w" mode overrides (and erases) the old content of the file.
 ?>
